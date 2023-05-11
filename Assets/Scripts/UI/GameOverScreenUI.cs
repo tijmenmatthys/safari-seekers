bbbd146e222629a3b67ac7cb84b95e75b6f7f5fe
JCho2002 <jason.cho@student.howest.be>
2023-05-11 15:40:27 +0200

    UPDATE Pause Screen and Minimap

--- Assets/Scripts/UI/GameOverScreenUI.cs
+++ Assets/Scripts/UI/GameOverScreenUI.cs
@@ -5,6 +5,7 @@ using TMPro;
 using JetBrains.Annotations;
 using static UnityEngine.Rendering.DebugUI;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 public class GameOverScreenUI : MonoBehaviour
 {
@@ -16,9 +17,16 @@ public class GameOverScreenUI : MonoBehaviour
     [SerializeField]
     private TMP_Text _scoreText;
 
+    [SerializeField]
+    private GameObject _gameOverFirstButton;
+
     public UnityEvent OnGameOver;
 
     private bool _gameOver = false;
+
+    private void Start()
+    {
+    }
 
     public void ShowGameOverScreen(float time, int score)
     {
@@ -26,6 +34,8 @@ public class GameOverScreenUI : MonoBehaviour
         {
             OnGameOver?.Invoke();
             _gameOverScreen.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(_gameOverFirstButton);
             int m = (int)(time / 60);
             int s = (int)(time % 60);
             _timeText.text = $"Total Playtime: " + $"{m:00}:{s:00}"; ;
