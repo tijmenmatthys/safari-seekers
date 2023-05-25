94f1b4ad5999aa3e5f52d7ad149146a55c5cf780
JCho2002 <jason.cho@student.howest.be>
2023-05-25 17:57:36 +0200

    UPDATE GameOver UI

--- Assets/Scripts/UI/GameOverScreenUI.cs
+++ Assets/Scripts/UI/GameOverScreenUI.cs
@@ -14,6 +14,8 @@ public class GameOverScreenUI : MonoBehaviour
     private TMP_Text _guessText;
     [SerializeField]
     private TMP_Text _nextMedalText;
+    [SerializeField]
+    private TMP_Text _medalText;
 
     [SerializeField]
     private Image _medalImage;
@@ -64,6 +66,7 @@ public class GameOverScreenUI : MonoBehaviour
             _glow.SetActive(false);
             _medalImage.sprite = null;
             _medalImage.gameObject.SetActive(false);
+            _medalText.text = "None";
             return "5+ Correct Guesses for Bronze!";
         }
         else if (score < 10)
@@ -71,6 +74,7 @@ public class GameOverScreenUI : MonoBehaviour
             _medalImage.sprite = _bronzeMedal;
             _medalImage.gameObject.SetActive(true);
             _glow.SetActive(true);
+            _medalText.text = "Bronze";
             return "10+ Correct Guesses for Silver!";
         }
         else if (score < 20)
@@ -78,6 +82,7 @@ public class GameOverScreenUI : MonoBehaviour
             _medalImage.sprite = _silverMedal;
             _medalImage.gameObject.SetActive(true);
             _glow.SetActive(true);
+            _medalText.text = "Silver";
             return "20+ Correct Guesses for Gold!";
         }
         else if (score >= 20 && totalGuesses != score)
@@ -85,12 +90,14 @@ public class GameOverScreenUI : MonoBehaviour
             _medalImage.sprite = _goldMedal;
             _medalImage.gameObject.SetActive(true);
             _glow.SetActive(true);
+            _medalText.text = "Gold";
             return "20+ Perfect Correct Guesses for Platinum!";
         }
         else
         {
             _medalImage.sprite = _platniumMedal;
             _medalImage.gameObject.SetActive(true);
+            _medalText.text = "Platinum";
             return "You are an Animal Master!";
         }
     }
